8ad5f8415d95025c9e71052955a4f0bc99dd0a87
Valentin Michalak <valentinmichalak@gmail.com>
2014-12-08 22:28:40 +0100

    init project

---
+++ index.js
@@ -0,0 +1,48 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+
+function downloader(pageNumber)
+{
+  var baseUrl = "http://unsplash.com";
+  var baseGridUrl = "/grid?page=";
+  var url = baseUrl + baseGridUrl + pageNumber;
+
+  request(url, function(error, response, html){
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      try { 
+        var nbPhotos = $('.photo>a').length;
+      } catch(err) {
+        var nbPhotos = 0;
+      }
+
+      $('.photo>a').each(function(i,elem){
+        var htmlUrl = $(this).attr('href');
+        var downloadUrl = baseUrl+htmlUrl;
+        var fileName = htmlUrl.replace('/photos/', '').replace('/download', '');
+
+        // Download Image
+        request(downloadUrl).on('response',  function (res) {
+          res.pipe(fs.createWriteStream('./photos/' + fileName + '.' + res.headers['content-type'].split('/')[1]));
+          console.log("Photo " + fileName + " downloaded.");
+        });
+        // End Download Image
+
+        //console.log(downloadUrl);
+      });
+
+      console.log("Page "+pageNumber+" scrapped: "+nbPhotos+" photos added to the waiting list.");
+
+      if(nbPhotos > 0)
+      {
+        downloader(pageNumber + 1);
+      }
+    }
+  });
+
+}
+
+//Start the script at the Page 1
+downloader(1);
